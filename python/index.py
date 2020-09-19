b688a306cb3bfee3c935223a40cc03d338a71ccf
minhtuan1407 <54676613+minhtuan1407@users.noreply.github.com>
2020-09-20 02:19:01 +0700

    Update index.py

--- python/index.py
+++ python/index.py
@@ -3,7 +3,7 @@ import os
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World Tuan!"
+    return "Hello World!"
     print(os.environ['DOCKER_TAG'])
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
