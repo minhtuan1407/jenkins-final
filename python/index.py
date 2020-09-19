18c2068a0f6277c859104fffe9d83acb7314436b
minhtuan1407 <54676613+minhtuan1407@users.noreply.github.com>
2020-09-20 01:09:19 +0700

    Update index.py

--- python/index.py
+++ python/index.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html', title='Docker Python', name='World')
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
