fddef4f3e17f28a38c5bd48f4fabbcb9f9867a7b
minhtuan1407 <54676613+minhtuan1407@users.noreply.github.com>
2020-09-20 00:15:37 +0700

    Update index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,11 +2,11 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const hostname = process.env.HOSTNAME || "unknown";
-const app_version = process.env.APP_VERSION || "unknown";
+const docker_tag = process.env.DOCKER_TAG || "unknown";
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = `Hello Word! Docker tag is ${app_version} - Hostname is ${hostname}\n`
+  const msg = `Hello Word! Docker tag is ${docker_tag} - Hostname is ${hostname}\n`
   res.end(msg);
 });
 
