a6bbf51e66daafddbf98c21e60a954b788277ef1
minhtuan1407 <54676613+minhtuan1407@users.noreply.github.com>
2020-09-20 09:27:03 +0700

    Update deploy-nodejs.sh

--- nodejs/deploy-nodejs.sh
+++ nodejs/deploy-nodejs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker pull minhtuan9801/nodejs
+docker pull minhtuan9801/nodejs:$DOCKER_TAG
 docker run -d --rm --name nodejs-app --net=host -e DOCKER_TAG=$DOCKER_TAG minhtuan9801/nodejs:$DOCKER_TAG
