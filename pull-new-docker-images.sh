88195ed299f5ff82774aab73b0f7fdcf85555415
minhtuan1407 <54676613+minhtuan1407@users.noreply.github.com>
2020-09-20 09:47:07 +0700

    Update pull-new-docker-images.sh

--- pull-new-docker-images.sh
+++ pull-new-docker-images.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-docker images | awk '(NR>1) && ($2!~/none/) {print $1":"$2}' | xargs -L1 docker pull
-docker stop $(docker ps -aq)
-
-./nodejs/deploy-nodejs.sh
-./python/deploy-python.sh
+if grep -Fqe "Image is up to date" << EOF
+`docker images | awk '(NR>1) && ($2!~/none/) {print $1":"$2}' | xargs -L1 docker pull`
+EOF
+then
+    echo "Notthing to update"
+    docker system prune --force
+else
+    echo "Newer image exist, redeploy!"
+    docker stop $(docker ps -aq)
+    ./nodejs/deploy-nodejs.sh
+    ./python/deploy-python.sh
+fi
