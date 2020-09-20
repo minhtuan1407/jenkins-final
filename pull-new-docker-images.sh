07b23e789654be065c4ad63a37c1b4541ff983a3
minhtuan1407 <54676613+minhtuan1407@users.noreply.github.com>
2020-09-20 10:11:15 +0700

    Update pull-new-docker-images.sh

--- pull-new-docker-images.sh
+++ pull-new-docker-images.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-if grep -Fqe "Image is up to date" << EOF
+if grep -Fqe "Image is up to date for minhtuan9801/python:latest" << EOF
 `docker images | awk '(NR>1) && ($2!~/none/) {print $1":"$2}' | xargs -L1 docker pull`
 EOF
 then
-    echo "Notthing to update"
-    docker system prune --force
+    echo "Python don't need to update"
 else
-    echo "Newer image exist, redeploy!"
+    echo "Newer python image exist, redeploy!"
+    docker stop $(docker ps -aq)
+    ./python/deploy-python.sh
+fi
+
+if grep -Fqe "Image is up to date for minhtuan9801/nodejs:latest" << EOF
+`docker images | awk '(NR>1) && ($2!~/none/) {print $1":"$2}' | xargs -L1 docker pull`
+EOF
+then
+    echo "Nodejs don't need to update"
+else
+    echo "Newer nodejs image exist, redeploy!"
     docker stop $(docker ps -aq)
     ./nodejs/deploy-nodejs.sh
-    ./python/deploy-python.sh
 fi
