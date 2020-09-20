8d4039c553f92199f09412138945449e489a0249
minhtuan1407 <54676613+minhtuan1407@users.noreply.github.com>
2020-09-20 10:45:53 +0700

    Update index.py

--- python/index.py
+++ python/index.py
@@ -6,6 +6,5 @@ os.environ['docker_tag'] = '$DOCKER_TAG'
 os.environ['hostname'] = '$HOSTNAME'
 def hello():
     return "Hello World Tuan!"
-#print("Hello World!", os.environ['docker_tag'], os.environ['hostname'])
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
