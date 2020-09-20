7710e77f1648b88bdc22dedc8055372ccfca9e81
minhtuan1407 <54676613+minhtuan1407@users.noreply.github.com>
2020-09-20 10:31:32 +0700

    Update index.py

--- python/index.py
+++ python/index.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template
 import os
 app = Flask(__name__)
 @app.route("/")
+os.environ['docker_tag'] = '$DOCKER_TAG'
+os.environ['hostname'] = '$HOSTNAME'
 def hello():
     return "Hello World !"
-    print(os.environ['DOCKER_TAG'])
+print("Hello World!", os.environ['docker_tag'])
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
